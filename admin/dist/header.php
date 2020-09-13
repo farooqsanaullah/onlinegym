1393f9b0da593e1c790da090c062fc3490984500
farooq980 <getjobs980@gmail.com>
2020-09-13 23:37:27 +0500

    modify code

--- admin/dist/header.php
+++ admin/dist/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
     <meta charset="utf-8" />
@@ -35,7 +35,7 @@
             <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                 <div class="sb-sidenav-menu">
                     <div class="nav">
-                        <a class="nav-link" href="index.html">
+                        <a class="nav-link" href="index.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Dashboard
                         </a>
@@ -118,100 +118,3 @@
 
             </nav>
         </div>
-        <div id="layoutSidenav_content">
-            <main>
-                <div class="container-fluid">
-                    <h1 class="mt-4">Dashboard</h1>
-                    
-                    <div class="row mt-table">
-                        <div class="col-xl-3 col-md-6">
-                            <div class="card bg-primary text-white mb-4">
-                                <div class="summary-title">
-                                    <i class="fa fa-user-circle" aria-hidden="true"></i> Total Users
-                                </div>
-                                <div class="card-body">
-                                   <h1>+100</h1>
-                                </div>
-                                
-                                <div class="card-footer d-flex align-items-center justify-content-between">
-                                    <a class="small text-white stretched-link" href="users.php">View Details</a>
-                                    
-                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-md-6">
-                            <div class="card bg-primary text-white mb-4">
-                                <div class="summary-title">
-                                    <i class="fa fa-user-circle" aria-hidden="true"></i> Total Braches
-                                </div>
-                                <div class="card-body">
-                                   <h1>+10</h1>
-                                </div>
-                                
-                                <div class="card-footer d-flex align-items-center justify-content-between">
-                                    <a class="small text-white stretched-link" href="branches.php">View Details</a>
-                                    
-                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-md-6">
-                            <div class="card bg-primary text-white mb-4">
-                                <div class="summary-title">
-                                    <i class="fa fa-user-circle" aria-hidden="true"></i> Total Classess
-                                </div>
-                                <div class="card-body">
-                                   <h1>+30</h1>
-                                </div>
-                                
-                                <div class="card-footer d-flex align-items-center justify-content-between">
-                                    <a class="small text-white stretched-link" href="classes.php">View Details</a>
-                                    
-                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-md-6">
-                            <div class="card bg-primary text-white mb-4">
-                                <div class="summary-title">
-                                    <i class="fa fa-user-circle" aria-hidden="true"></i> Total Trainer
-                                </div>
-                                <div class="card-body">
-                                   <h1>+20</h1>
-                                </div>
-                                
-                                <div class="card-footer d-flex align-items-center justify-content-between">
-                                    <a class="small text-white stretched-link" href="trainer.php">View Details</a>
-                                    
-                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                </div>
-            </main>
-            <footer class="py-4 bg-light mt-auto">
-                <div class="container-fluid">
-                    <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; 2020</div>
-
-                    </div>
-                </div>
-            </footer>
-        </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"
-        crossorigin="anonymous"></script>
-    <script src="js/scripts.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-    <script src="assets/demo/chart-area-demo.js"></script>
-    <script src="assets/demo/chart-bar-demo.js"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-    <script src="assets/demo/datatables-demo.js"></script>
-</body>
-
-</html>
---
+++ admin/dist/index.php
@@ -0,0 +1,87 @@
+<?php  include 'header.php';   ?>
+  
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid">
+                    <h1 class="mt-4">Dashboard</h1>
+                    
+                    <div class="row mt-table">
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="summary-title">
+                                    <i class="fa fa-user-circle" aria-hidden="true"></i> Total Users
+                                </div>
+                                <div class="card-body">
+                                   <h1>+100</h1>
+                                </div>
+                                
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <a class="small text-white stretched-link" href="users.php">View Details</a>
+                                    
+                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="summary-title">
+                                    <i class="fa fa-user-circle" aria-hidden="true"></i> Total Braches
+                                </div>
+                                <div class="card-body">
+                                   <h1>+10</h1>
+                                </div>
+                                
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <a class="small text-white stretched-link" href="branches.php">View Details</a>
+                                    
+                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="summary-title">
+                                    <i class="fa fa-user-circle" aria-hidden="true"></i> Total Classess
+                                </div>
+                                <div class="card-body">
+                                   <h1>+30</h1>
+                                </div>
+                                
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <a class="small text-white stretched-link" href="classes.php">View Details</a>
+                                    
+                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="summary-title">
+                                    <i class="fa fa-user-circle" aria-hidden="true"></i> Total Trainer
+                                </div>
+                                <div class="card-body">
+                                   <h1>+20</h1>
+                                </div>
+                                
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <a class="small text-white stretched-link" href="trainer.php">View Details</a>
+                                    
+                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; 2020</div>
+
+                    </div>
+                </div>
+            </footer>
+        </div> 
+        
+<?php include 'footer.php';?>
